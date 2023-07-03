cd335bd0f57a248661518cd84d947a23573ffab7
Asif <asif786ka@gmail.com>
2023-07-03 14:56:39 +0530

    Initial commit

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const API_KEY = '9dc4772f29bc8ea6815d08313e064148';
+export const BASE_URL = 'https://api.themoviedb.org/3';
+export const IMAGE_POSTER_URL = 'https://image.tmdb.org/t/p/original';
+export const POSTER_IMAGE = 'https://image.tmdb.org/t/p/w200';
